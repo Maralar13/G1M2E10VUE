0d22d19fe038cc7636c5ade1e85a87d5bfd9b09e
MichelCespedes <72767188+MichelCespedes@users.noreply.github.com>
2020-12-16 12:35:58 -0500

    components/

---
+++ documento.vue
@@ -0,0 +1,35 @@
+<template>
+    Documento
+</template>
+
+
+<script>
+export default {
+  name: 'Documento',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+    #documento{
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;    
+        align-items: center;
+    }
+
+    #documento h2{
+        font-size: 50px;
+        color: #283747;
+    }
+
+    #documento span{
+        color: crimson;
+        font-weight: bold;
+    }
+</style>
